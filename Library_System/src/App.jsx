15e7dba43f077d600e61e60da2c0eff261e399ac
Helayel Hamayel <helayelhamayel@gmail.com>
2026-08-12 13:02:32 +0300

    first commit, placing basic files structure

---
+++ Library_System/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <section>
+        <header>
+          
+        </header>
+        <main>
+          
+        </main>
+      </section>
+  )
+}
+
+export default App
---
+++ Library_System/src/Pages/BooksCatalogue.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function BooksCatalogue() {
+  return (
+    <div>Dashboard</div>
+  )
+}
